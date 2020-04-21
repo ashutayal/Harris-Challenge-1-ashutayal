719a072c676c782d893926b6d2a994d4f7f2f041
ashutayal <54512634+ashutayal@users.noreply.github.com>
2020-04-21 05:10:44 -0500

    Solutions

---
+++ challenge_1.py
@@ -0,0 +1,50 @@
+# CHALLENGE PROBLEMS
+# YOU MAY NOT USE ANY ADDITIONAL LIBRARIES OR PACKAGES TO COMPLETE THIS CHALLENGE
+
+# ASHU TAYAL
+
+# Divvy is Chicago's bike share system, which consists of almost 600 stations scattered
+# around the city with blue bikes available for anyone to rent. Users begin a ride by removing
+# a bike from a dock, and then they can end their ride by returning the bike to a dock at any Divvy 
+# station in the city. You are going to use real data from Divvy collected at 1:30pm on 4/7/2020 
+# to analyze supply and demand for bikes in the system. 
+
+# NOTE ** if you aren't able to run this, type "pip install json" into your command line
+import json
+
+# do not delete; this is the data you'll be working with
+divvy_stations = json.loads(open('/Users/ashutayal/Documents/GitHub/Harris-Challenge-1/divvy_stations.txt').read())
+
+# PROBLEM 1
+# find average number of empty docks (num_docks_available) and 
+# available bikes (num_bikes_available) at all stations in the system
+
+total_empty_docks = 0
+for i in range(len(divvy_stations)):
+    total_empty_docks += divvy_stations[i]['num_docks_available']
+
+print("Average # of empty docks:", total_empty_docks/len(divvy_stations))
+
+available_bikes = 0
+for i in range(len(divvy_stations)):
+    available_bikes += divvy_stations[i]['num_bikes_available']
+
+print("Total # of bikes across all docks:", available_bikes)
+
+# PROBLEM 2
+# find ratio of bikes that are currently rented to total bikes in the system (ignore ebikes)
+
+# Assuming empty dock means rented bike.
+print("The required ratio is:", total_empty_docks/(total_empty_docks+available_bikes))
+
+# PROBLEM 3 
+# Add a new variable for each divvy station's entry, "percent_bikes_remaining", that shows 
+# the percentage of bikes available at each individual station (again ignore ebikes). 
+# This variable should be formatted as a percentage rounded to 2 decimal places, e.g. 66.67%
+
+def updater(dict):
+    a = dict['num_bikes_available']/(dict['num_bikes_available']+dict['num_docks_available'])
+    return "{:.2%}".format(a)
+
+for i in range(len(divvy_stations)):
+    divvy_stations[i].update({'percent_bikes_remaining': updater(divvy_stations[i])})
